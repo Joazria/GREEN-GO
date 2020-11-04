03edecbb943ca0fbd7a7aa146fa120f426fdd9e9
JoA Azria <joa.azria@gmail.com>
2020-11-04 16:15:12 -0300

    cloudinary install

--- app/controllers/band_controller.rb
+++ app/controllers/band_controller.rb
@@ -1,2 +1,7 @@
 class BandController < ApplicationController
 end
+
+def article_params
+  params.require(:band).permit(:name, :release, :website, :facebook, :instagram, :youtube, :spotify, :soundcloud, :email, :local, :team, :photo)
+end
+
--- app/models/band.rb
+++ app/models/band.rb
@@ -1,5 +1,6 @@
 class Band < ApplicationRecord
   belongs_to :user
+  has_one_attached :photo
   validates :name, uniqueness: true
   validates :website, :facebook, :instagram, :youtube, :soundcloud, :spotify, :local, presence: true
   validates :email, format: { with: /^(.+)@(.+)$/, message: "Email invalid"  }
