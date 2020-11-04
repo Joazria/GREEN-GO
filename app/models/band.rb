c1a3768dd9ce655f76987cb159b7b7c8fdef66ff
JoA Azria <joa.azria@gmail.com>
2020-11-04 13:40:02 -0300

    creation model band and validation

---
+++ app/models/band.rb
@@ -0,0 +1,8 @@
+class Band < ApplicationRecord
+  belongs_to :user
+  validates :name, uniqueness: true
+  validates :website, :facebook, :instagram, :youtube, :soundcloud, :spotify, :local, presence: true
+  validates :email, format: { with: /^(.+)@(.+)$/, message: "Email invalid"  }
+  validates :team, presence: true, numericality: { only_integer: true }
+  validates :availability, presence: true, :inclusion => { :in => [true, false] }
+end
